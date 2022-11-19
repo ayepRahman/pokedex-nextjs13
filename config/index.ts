16a9813b9fb18b2b1c9fe5ae11a3d62d48a2e6f7
Arif Rahman <matrep88@gmail.com>
2022-11-20 00:21:59 +0800

    fixing env var

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,4 @@
-// TODO: check base on process.env.NODE_ENV
-
-export const BASE_API_URL =
-  process.env.ONLYAYEP_BASE_API_URL || "http://localhost:3000/api";
+export const BASE_API_URL = process.env.NEXT_PUBLIC_ONLYAYEP_BASE_API_URL;
 export const AWS_ACCESS_KEY = process.env.ONLYAYEP_AWS_ACCESS_KEY || "";
 export const AWS_SECRET_KEY = process.env.ONLYAYEP_AWS_SECRET_KEY || "";
 export const S3_BUCKET_NAME = process.env.ONLYAYEP_S3_BUCKET_NAME || "";
